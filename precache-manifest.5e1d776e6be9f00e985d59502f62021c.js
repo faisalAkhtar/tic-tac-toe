9f7331c8747eefd05aa5611bc1062e0b1fb3419d
Faisal Akhtar PC <faisalakhtar1999@gmail.com>
2020-08-20 07:26:42 +0530

    Updates

---
+++ precache-manifest.5e1d776e6be9f00e985d59502f62021c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09f1973ec01d58b0c8761bd49580a73",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "2eb223af6e8e69d9dc01",
+    "url": "/tic-tac-toe/static/css/main.e261eaa4.chunk.css"
+  },
+  {
+    "revision": "a2436778992cd372315b",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eb223af6e8e69d9dc01",
+    "url": "/tic-tac-toe/static/js/main.4445ddcd.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
