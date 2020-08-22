6add1644f99cd519bbfbb092e70107750f3e7dac
Faisal Akhtar PC <faisalakhtar1999@gmail.com>
2020-08-22 19:21:22 +0530

    ‎

--- src/index.js
+++ src/index.js
@@ -34,6 +34,10 @@ class Board extends React.Component {
     });
   }
 
+  restart() {
+    window.location.reload(false)
+  }
+
   renderSquare(i) {
     return (
       <Square
@@ -54,25 +58,39 @@ class Board extends React.Component {
         status = 'Winner: '
         sel = winner
       }
+      return (
+        <div>
+          <div className="board-row">
+            {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
+          </div>
+          <div className="status">{status} <span>{sel}</span></div>
+          <button class="restart" onClick={() => this.restart()}>Restart</button>
+        </div>
+      );
     } else {
       status = 'Next player: ';
       sel = (this.state.isNext ? 'X' : 'O')
+      return (
+        <div>
+          <div className="board-row">
+            {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
+          </div>
+          <div className="status">{status} <span>{sel}</span></div>
+        </div>
+      );
     }
-
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
-        </div>
-        <div className="status">{status} <span>{sel}</span></div>
-      </div>
-    );
   }
 }
 
